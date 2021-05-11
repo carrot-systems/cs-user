f8417ea7db6060d9f273ae5cfaed5f84f0a874d8
Alice Di Nunno <alice@dinunno.fr>
2021-05-11 20:14:03 +0200

    [debug] added postgres logger

--- src/adapters/persistence/postgres/postgres.go
+++ src/adapters/persistence/postgres/postgres.go
@@ -19,7 +19,10 @@ func StartGormDatabase(config config.GormConfig) *gorm.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		config.Host, config.Port, config.User, config.Password, config.DbName)
-	db, err := gorm.Open(pg.Open(psqlInfo), &gorm.Config{})
+	db, err := gorm.Open(pg.Open(psqlInfo), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
 	if err != nil {
 		panic(err)
 	}
