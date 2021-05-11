e2958d65ab1c6752d5a0fe8d65c2ec19bce077f5
Alice Di Nunno <alice@dinunno.fr>
2021-05-11 21:23:04 +0200

    [bug] fix wrong handle

--- src/core/domain/permission.go
+++ src/core/domain/permission.go
@@ -6,6 +6,6 @@ var ( //This user can:
 	PermEditUser       = 2 //Edit created users
 	PermDeleteUser     = 4 //Delete created users
 	PermReadAllUser    = 8 //Read an user profile
-	PermFullUserAccess = PermCreateUser & PermEditUser & PermDeleteUser & PermReadAllUser
+	PermFullUserAccess = PermCreateUser | PermEditUser | PermDeleteUser | PermReadAllUser
 )
 var UserPermissionIdentifier = "permissions.usermanagement"
