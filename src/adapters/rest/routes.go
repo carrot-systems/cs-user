fd13e84056f8ca74b6791f45b22e2bc909c9aaa1
Alice Di Nunno <alice@dinunno.fr>
2021-05-11 21:51:55 +0200

    [improv] reducing string repetitions

--- src/adapters/rest/routes.go
+++ src/adapters/rest/routes.go
@@ -3,9 +3,10 @@ package rest
 import "github.com/gin-gonic/gin"
 
 func SetRoutes(r *gin.Engine, routesHandler RoutesHandler) {
-	r.POST("/users", routesHandler.CreateUserHandler)
-	authenticatedRoutes := r.Use(routesHandler.GetUserMiddleware)
-	authenticatedRoutes.GET("/users/:handle", routesHandler.GetProfileHandler)
-	authenticatedRoutes.PUT("/users/:handle", routesHandler.EditProfileHandler)
-	authenticatedRoutes.DELETE("/users/:handle", routesHandler.RemoveUserHandler)
+	group := r.Group("/users")
+	group.POST("/", routesHandler.CreateUserHandler)
+	authenticatedRoutes := group.Use(routesHandler.GetUserMiddleware)
+	authenticatedRoutes.GET("/:handle", routesHandler.GetProfileHandler)
+	authenticatedRoutes.PUT("/:handle", routesHandler.EditProfileHandler)
+	authenticatedRoutes.DELETE("/:handle", routesHandler.RemoveUserHandler)
 }
