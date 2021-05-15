0573bcca8f84987d3ef86fdf69f20aed5d898ca8
Alice Di Nunno <alice@dinunno.fr>
2021-05-15 10:48:42 +0200

    [improv] added data to response

--- src/core/domain/responseStatus.go
+++ src/core/domain/responseStatus.go
@@ -1,6 +1,7 @@
 package domain
 
 type Status struct {
-	Success bool   `json:"success"`
-	Message string `json:"message"`
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
